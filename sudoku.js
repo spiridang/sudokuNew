8533c44dff10211a45e222f34a1e5a6db8226b96
Ayur67 <aur67@mail.ru>
2023-12-08 14:29:18 +0300

    arrayOne

--- sudoku.js
+++ sudoku.js
@@ -10,13 +10,6 @@ function solve() {
   const strokes = arrSudoku.map((el) => el.split(""));
   const one = strokes[0];
 
-  // for (let i = 0; i < one.length; i += 1) {
-  //   if (one[i] === "-") {
-  //     one[i] = NaN;
-  //   }
-  // }
-  // console.log(one);
-  // return one;
   let newArr = [];
   for (let i = 0; i < one.length; i += 1) {
     let arrIn = one.splice(0, 9);
@@ -26,6 +19,8 @@ function solve() {
 }
 const arraysOne = solve();
 console.log(arraysOne);
+
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции read.
  * Возвращает игровое поле после попытки его решить.
